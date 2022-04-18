e03a948cd0ced21f14b1f66522b81f57a1d5474e
shubham1769 <skavale33@gmail.com>
2022-04-18 18:10:41 +0530

    sdsd

--- pipelineUtils.groovy
+++ pipelineUtils.groovy
@@ -24,9 +24,9 @@ def restGet(url, credentialId, contentType = 'APPLICATION_JSON') {
 }
 
 def restPost(url, credentialId, body, contentType = 'APPLICATION_JSON') {
-    res = httpRequest url: url, contentType: contentType, customHeaders: [Bearer: credentialId], httpMode: 'POST', requestBody: body, consoleLogResponseBody: true
-    println res.getStatus()
-    res.getContent()
+    // res = httpRequest url: url, contentType: contentType, customHeaders: [Bearer: credentialId], httpMode: 'POST', requestBody: body, consoleLogResponseBody: true
+    // println res.getStatus()
+    // res.getContent()
     sh "curl -u ${jfrogCred} --header 'Content-Type: application/json' data ${body} ${url}"
 }
 
